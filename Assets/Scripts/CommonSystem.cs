732aa9d057c171a795146bd18b07416ed103477b
ngc <0507nog@gmail.com>
2026-05-12 08:42:35 +0900

    escキーでゲームを終了するコマンドを全シーンで行う

---
+++ Assets/Scripts/CommonSystem.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CommonSystem : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var kb = Keyboard.current;
+        if (kb.escapeKey.IsPressed())
+        {
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+        }
+    }
+}
